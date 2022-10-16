69fe4986a2c0f204d2ced891e187a246769e8b21
sodutu <stephenodutu@gmail.com>
2022-10-15 22:28:44 -0700

    ist one

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always o
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s) \n", size of  (char));
+	printf("Size of an int: %zu byte(s) \n", size of  (int));
+	printf("Size of a long int: %zu byte(s) \n", size of  (long int));
+	printf("Size of a long long int: %zu byte(s) \n", size of  (long long int));
+	printf("Size of a float: %zu byte(s) \n", size of  (float));
+	return (0);
+}
