ecadb3a2853a1197fd3c931eaf9068d551905951
sodutu <107021001+sodutu@users.noreply.github.com>
2022-11-26 14:10:18 +0300

    Update utils.c

--- printf/utils.c
+++ printf/utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -15,16 +15,16 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal code to buffer
- * @buffer: Array of chars.
- * @i: Index at which to start appending.
- * @ascii_code: ASSCI CODE.
- * Return: Always 3
+ * append_hexa_code - Append ascii in hexadecimal code to buffer
+ * @buffer: Array of chars
+ * @i: Index at which to start appending
+ * @ascii_code: ASCII CODE.
+ * Return: Always 3.
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
-	/* The hexa format code is always 2 digits long */
+	/* The hexa format is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
@@ -53,8 +53,8 @@ int is_digit(char c)
 
 /**
  * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  *
  * Return: Casted value of num
  */
