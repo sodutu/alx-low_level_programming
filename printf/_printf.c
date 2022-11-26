4a7d61c10237200676d0f818aba3d3a70bb1dbde
sodutu <107021001+sodutu@users.noreply.github.com>
2022-11-26 13:36:06 +0300

    Update _printf.c

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * @format - format.
+ * Return: printed chars
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -37,30 +37,26 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
+			printed = handle_print(format, &i, list, buffer, flags, width,
+precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
 		}
 	}
-
 	print_buffer(buffer, &buff_ind);
-
 	va_end(list);
-
 	return (printed_chars);
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - prints the contents of the buffer if it exists
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index at which to add next char, represents the length
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
-
 	*buff_ind = 0;
 }
